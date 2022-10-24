6d60498e1f2efbf1481dbd281167ff2cdcc1e842
Dorian Turgot <dorian.turgot@gmail.com>
2022-10-24 16:30:12 +0200

    added tensorflow to requierements

--- dorian_reconone.py
+++ dorian_reconone.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import cvlib as cv
+import tensorflow
 from cvlib.object_detection import draw_bbox
 from numpy.lib.polynomial import poly
 
