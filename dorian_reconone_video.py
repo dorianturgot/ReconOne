cd1b2024af947f99f2a6932a3b21a97297c9f024
Dorian Turgot <dorian.turgot@gmail.com>
2022-11-21 10:29:45 +0100

    l'algo renvoie une moyenne et compte que les nouvelles personnes

--- dorian_reconone_video.py
+++ dorian_reconone_video.py
@@ -3,26 +3,54 @@ import cv2
 
 import cvlib as cv
 import os
+import math
 
-def countPeople():
+def countPeople(percentage = 500):
     capture = cv2.VideoCapture("video1.mp4")
     frame_count = capture.get(cv2.CAP_PROP_FRAME_COUNT)
-    framerate = frame_count / 100
+    print("framecount = " + str(frame_count))
+    print("to exit press Q")
+    framerate = frame_count / percentage
     nbPersonnes = []
     if capture:
-        while True:
+        oldbox = []
+        while framerate < frame_count:
             ret, frame = capture.read()
             if ret:
                 box, label, count = cv.detect_common_objects(frame)
                 # print le nb de personnes
                 #print(len(label))
-                nbPersonnes.append(len(label))        
+                """print("OLDBOX")
+                print(oldbox)
+                print("BOX")
+                print(box)"""
+                
+                # multiplier le nombre obtenu par la confiance de cet élément
+                
+                totalNewPersonOnFrame = 0
+                for i in range (len(box)):
+                    """ Vérifie que les nouvelles personnes comptées n'ont pas déjà été comptées en vérifiant
+                    la distance avec leur ancienne position """
+                    #if i >= len(oldbox) or (sum(box[i]) + sum(oldbox[i])) > 250:
+                    if i >= len(oldbox) or (math.sqrt((oldbox[i][0] - box[i][0])**2)) > 350:
+                        """if framerate > 30 and i < len(oldbox):
+                            print("-------")
+                            print((math.sqrt((oldbox[i][0] - box[i][0])**2)))
+                            print("------------")"""
+                        if label[i] == "person" :
+                            totalNewPersonOnFrame += round(count[i], 3)
+                            
+                
+                nbPersonnes.append(totalNewPersonOnFrame)
                 cv2.imshow("Track", frame)
                 framerate += 30
                 capture.set(cv2.CAP_PROP_POS_FRAMES, framerate)
+                
+                oldbox = box
             key = cv2.waitKey(100)
+            
             if key == ord('q'):
                     break
-    return nbPersonnes
+    return nbPersonnes, frame_count, percentage
 
 print(countPeople())
