8b750b2b87d02ee94fa1dcce7b9b6e8f4f625da4
Dorian Turgot <dorian.turgot@gmail.com>
2022-12-05 15:24:46 +0100

    web

---
+++ RecoSite/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+UPLOAD_FOLDER = 'static//uploads//'
+EXTENSIONS = {'mp4'}
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# 16 * 1000 * 1000 = 16 Mo
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 * 1024
