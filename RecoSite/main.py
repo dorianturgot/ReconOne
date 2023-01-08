9a4a44258183f8129e9e5e74c06985f62f49d3d5
Dorian Turgot <dorian.turgot@gmail.com>
2023-01-08 20:49:24 +0100

    nouveau design pour le site

--- RecoSite/main.py
+++ RecoSite/main.py
@@ -13,22 +13,6 @@ from analyse import *
 
 from flask import Flask, render_template
 
-
-def test():
-    # Algorithme de Gram-Schmidt 
-    v1 = np.array([2 / 3, 2 / 3, 1 / 3])
-    v2 = np.array([-2 / 3, 1 / 3, 2 / 3])
-    v3 = np.array([1 / 3, -2 / 3, 2 / 3])
-
-    u1 = v1 / np.linalg.norm(v1)
-    u2 = v2 - np.dot(u1, v2) * u1
-    u2 /= np.linalg.norm(u2)
-    u3 = v3 - np.dot(u1, v3) * u1 - np.dot(u2, v3) * u2
-    u3 /= np.linalg.norm(u3)
-
-    return np.linalg.norm(u1), np.linalg.norm(u2), np.linalg.norm(u3)
-
-
 @app.route('/')
 def menu():
     return render_template("index.html")
@@ -37,15 +21,13 @@ def menu():
 @app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        vectors = test()
+        pass
     f = request.files['file']
     if f.filename == '':
         flash('Pas de vidéo sélectionnée')
         return redirect(request.url)
     f.save(secure_filename("video.mp4"))
-    #flash('Video uploadée')
     return redirect(url_for("traitement"))
-    #return render_template("stats.html", vectors=vectors, f=f)
 
 
 @app.route('/index.html')
